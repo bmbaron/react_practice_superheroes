d76d67cbf9844c0f2d0f35db4335a4cef46309d9
bmbaron <benjamin.m.baron@gmail.com>
2022-05-31 08:54:44 +0700

    changes to design of header for responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ export default function Header(props) {
 	return (
 			<header className="header">
 				<div className="name">
-					superheroes are cool
+					<h1>
+						superheroes
+					</h1>
 				</div>
 				<button className="main-button" onClick={props.function}>
 					{props.shown ? "hide all" : "show all"}
